e18ca4f5a8d325de553f10cc07c669b4423bbd1e
gihwanJang <90659997+gihwanJang@users.noreply.github.com>
2024-07-08 01:36:08 +0900

    feat : init

---
+++ src/main/java/com/nhnacademy/servicereview_v2/ServiceReviewV2Application.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.servicereview_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceReviewV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceReviewV2Application.class, args);
+    }
+
+}
